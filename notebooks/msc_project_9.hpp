fe15d56b9143a15714ac73c68b040b0d423dc49c
Paul Johnson <p.johno@gmail.com>
2021-07-08 16:50:35 +0100

    include hpp setup files for notebooks

---
+++ notebooks/msc_project_9.hpp
@@ -0,0 +1,16 @@
+// load libraries
+#include "msc_cling_libs_loader.hpp"
+
+// include standard stuff
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+// for plotting
+#include "msc_gnuplot_image.hpp"
+
+using namespace MATH60082;
+
