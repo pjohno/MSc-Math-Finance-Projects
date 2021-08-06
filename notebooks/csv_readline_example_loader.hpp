64b1ee69647fa80cea350010a664f05ba61143e0
Paul Johnson <p.johno@gmail.com>
2021-08-06 17:06:45 +0100

    Add CSV example

---
+++ notebooks/csv_readline_example_loader.hpp
@@ -0,0 +1,14 @@
+// load libraries
+#include "msc_cling_libs_loader.hpp"
+
+// include standard stuff
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+// project specific stuff
+#include <math60082_csv_readline.hpp>
